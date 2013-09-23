05f206e64c91db25c95254521a9fe426936daeb7
Noah Alonso-Torres <noahat4004@gmail.com>
2013-09-23 13:57:58 -0700

    Add Main Navigation Bar to Application Views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,14 @@ class ApplicationController < ActionController::Base
   include Clearance::Controller
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :collect_artists
   protect_from_forgery with: :exception
   
   private
+  
+  def collect_artists
+    @artists = Artist.all
+  end
   
   def is_logged_in?
     if user.nil?
