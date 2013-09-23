dfdf401241f8902fcb84a9ca48856475a1a15202
Noah Alonso-Torres <noahat4004@gmail.com>
2013-09-23 14:29:28 -0700

    Add Footer to Application Views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,17 @@ class ApplicationController < ActionController::Base
   include Clearance::Controller
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  before_filter :collect_artists
+  before_filter :collect_artists, :collect_company_info
   protect_from_forgery with: :exception
   
   private
   
   def collect_artists
     @artists = Artist.all
+  end
+  
+  def collect_company_info
+    @company_info = Ewmanagement.first
   end
   
   def is_logged_in?
