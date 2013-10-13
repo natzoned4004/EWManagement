d170b45aaae5f171b7d752c5438a3518aa0ea3b0
Noah Alonso-Torres <noahat4004@gmail.com>
2013-10-13 14:24:13 -0700

    Add Delete to Admin Bar when editting Artist

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,9 @@ module ApplicationHelper
     path == '/sign_in' ||
     path == '/sign_up'
   end
+  
+  def edit_artist?
+    path = request.fullpath
+    path["artists"] && path["edit"]
+  end
 end
