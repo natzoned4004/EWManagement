1e70075b4a7d3bb79cfc7e5958820f9c5578763a
Noah Alonso-Torres <noahat4004@gmail.com>
2013-10-13 12:50:23 -0700

    Add Fixed Footer to Sign_In and Sign_Up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@ module ApplicationHelper
   def home?
     request.fullpath == '/'
   end
+  
+  def fixed_footer?
+    path = request.fullpath
+    path == '/' ||
+    path == '/sign_in' ||
+    path == '/sign_up'
+  end
 end
